63fd0122e8da31f7dec6832fd42ca641f2a8b0b4
rfigueroa <41128440+rfigueroa@users.noreply.github.com>
2021-06-09 11:01:33 -0500

    initial commit

---
+++ src/main/java/com/example/dependenciesconstraints/DependenciesConstraintsApplication.java
@@ -0,0 +1,13 @@
+package com.example.dependenciesconstraints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependenciesConstraintsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependenciesConstraintsApplication.class, args);
+	}
+
+}
